1cba42e39a64146761e4fd983a2a44e1e8e84f7a
Edgar Cruz <edgaar.cruz@outlook.com>
2020-05-28 00:17:54 -0600

    avances navbar

--- src/components/layout/navbar.jsx
+++ src/components/layout/navbar.jsx
@@ -6,22 +6,24 @@ import Form from 'react-bootstrap/Form'
 import FormControl from 'react-bootstrap/FormControl'
 import '../../App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
-
+import Logo from '../../resources/images/layout/asesorauto.png'
 const App = () => (
-    <Navbar bg="light" expand="md" className="navbar" variant="dark">
-        <Navbar.Brand href="#home">Asesorauto Logo</Navbar.Brand>
+    <Navbar expand="md" className="navbar">
+        <Navbar.Brand href="#home" ><img src={Logo} className="navbar-logo"></img></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto">
-                    <Nav.Link href="#home" className="link">CATALAGO</Nav.Link>
-                    <Nav.Link href="#link">SOFTWARE</Nav.Link>
-                    <Nav.Link href="#link">TALLER</Nav.Link>
-                    <Nav.Link href="#link">CAPACITACIONES</Nav.Link>
-                    <Nav.Link href="#link">CONTACTO</Nav.Link>
-                    <FontAwesomeIcon icon="search" size="lg" className="icon"/>
-                    <FontAwesomeIcon icon="shopping-cart" size="lg" className="icon"/>
+                    <Nav.Link href="#home" className="navlink">CATALAGO</Nav.Link>
+                    <Nav.Link href="#link" className="navlink">SOFTWARE</Nav.Link>
+                    <Nav.Link href="#link" className="navlink">TALLER</Nav.Link>
+                    <Nav.Link href="#link" className="navlink">CAPACITACIONES</Nav.Link>
+                    <Nav.Link href="#link" className="navlink">CONTACTO</Nav.Link>
                 </Nav>
+                <Form inline>
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2 navlink" />
+                    <Button variant="info">Search</Button>
+                </Form>
+                <FontAwesomeIcon icon="shopping-cart" size="lg" className="icon navlink"/>
             </Navbar.Collapse>
     </Navbar>
 );
